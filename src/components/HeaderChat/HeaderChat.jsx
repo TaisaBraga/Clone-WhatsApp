8cab6285862bcd8160dfa8cd2e8b431d551e1b45
Taisa Braga Bento <taisabraga.adm@gmail.com>
2022-07-28 15:49:22 -0300

    create Header component

--- src/components/HeaderChat/HeaderChat.jsx
+++ src/components/HeaderChat/HeaderChat.jsx
@@ -1,9 +1,16 @@
+import ChatIcon from '@material-ui/icons/Chat';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+import HeaderPhoto from "../../images/meredith.jpg"
 import './HeaderChat.css'
 
 export default function HeaderChat(){
     return(
         <div className="headerChat">
-            <h1>page</h1>
+            <img src={HeaderPhoto} alt="" className='PersonImage'/>
+            <div className='Icons'>
+            <ChatIcon />
+            <MoreVertIcon className='VertIcon'/>
+            </div>
         </div>
     )
 }
