028b40b468667514b5070996f9a53b715134fc96
Taisa Braga Bento <taisabraga.adm@gmail.com>
2022-07-29 15:59:19 -0300

    Call the API message

--- src/pages/WhatsAppMessages/WhatsAppMessages.jsx
+++ src/pages/WhatsAppMessages/WhatsAppMessages.jsx
@@ -1,8 +1,18 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import MessageHeader from "../../components/MessageHeader";
+import { getMessage } from "../../services/WhatsAppAPIService";
 import "./WhatsAppMessages.css";
 
 export default function WhatsAppMessages() {
+  const [messages, setMessages] = useState([]);
+
+  useEffect(() => {
+    getMessage().then((response) => {
+      console.log(response);
+      setMessages(response);
+    });
+  }, []);
+
   return (
     <div className="WhatsAppMessages">
       <div className="header">
